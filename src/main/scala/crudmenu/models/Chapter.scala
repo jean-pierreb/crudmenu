96a2e4d9449542ff6c44a8601d6f7c411c99e854
bakaman <bakaman@zorgdomein.nl>
2014-10-01 13:50:14 +0200

    improved chapter model

--- src/main/scala/crudmenu/models/Chapter.scala
+++ src/main/scala/crudmenu/models/Chapter.scala
@@ -1,3 +1,8 @@
 package crudmenu.models
+import spray.json._
 
-case class Chapter(id: String, name: String)
+case class Chapter(id: String, name: String, category: List[String] = List(), healthcareRequest: List[String] = List())
+
+object Chapter extends DefaultJsonProtocol{
+  implicit val format = jsonFormat4(Chapter.apply)
+}
